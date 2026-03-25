96e80570e40dc5d0d10557bf3f3510893576f4a5
Heathorian <fusiontoto2001@gmail.com>
2026-03-25 14:42:13 +0100

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,6 +1,7 @@
 // This file ensures TypeScript understands `import.meta.env` (Vite).
 interface ImportMetaEnv {
   readonly VITE_FMP_API_KEY?: string;
+  readonly VITE_ALPHA_VANTAGE_API_KEY?: string;
 }
 
 interface ImportMeta {
